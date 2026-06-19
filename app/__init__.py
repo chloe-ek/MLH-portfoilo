f7cbc422d38ffd8472776cd543575d45654958bc
chloe-ek <chloe.kwon08@gmail.com>
2026-06-19 10:49:10 -0700

    Add profile photo and about section

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    about = {
+        "name": "Chloe",
+        "bio": "Hi! I'm Chloe, a software developer and MLH Fellow based in Vancouver. I love building things for the web and learning new technologies. Outside of coding, I enjoy exploring new places and trying new foods."
+    }
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), about=about)
